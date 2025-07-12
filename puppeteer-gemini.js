0306b30a12fc52eb656fe7755d04e208b0ec2a98
hani9999 <58763768+hani9999@users.noreply.github.com>
2025-07-12 09:51:33 +0200

    Update puppeteer-gemini.js

--- puppeteer-gemini.js
+++ puppeteer-gemini.js
@@ -1,7 +1,47 @@
 const puppeteer = require("puppeteer");
 const fs = require("fs");
 
-const cookies = [ /* ... نسخ الكوكيز تبعك كما هي هنا ... */ ];
+const cookies = [
+  {
+    name: "AEC",
+    value: "AVh_V2jqpHvjdbifLwLHSejVVy3yNiDUwEtMj1OR1gpe6KfRNEE3Bd4tVg",
+    domain: ".google.com",
+    path: "/",
+    httpOnly: true,
+    secure: true
+  },
+  {
+    name: "APISID",
+    value: "OVODRZ-BibfWRtSP/A1wlgHzPr1VL_xSIs",
+    domain: ".google.com",
+    path: "/",
+    secure: true
+  },
+  {
+    name: "SAPISID",
+    value: "E4ZHqgEnNxmLbN6l/Apj1csRX4-Wo80KJM",
+    domain: ".google.com",
+    path: "/",
+    secure: true
+  },
+  {
+    name: "SID",
+    value: "g.a000ywjcq5kOO45l5KnImc9v9gW5eGQfmODE-eRvJpmO1fB4RgzQtzH3jjw5ZE4iiUTgoAHQzAACgYKAesSARYSFQHGX2MiTOUHzAESatQQCCw2GN04qRoVAUF8yKr0c0GkVwjYz5MdBJ_AWV6R0076",
+    domain: ".google.com",
+    path: "/",
+    httpOnly: true,
+    secure: true
+  },
+  {
+    name: "__Secure-1PSID",
+    value: "g.a000ywjcq5kOO45l5KnImc9v9gW5eGQfmODE-eRvJpmO1fB4RgzQUOdYxyCYYi77Uo4uWAeZcQACgYKAdASARYSFQHGX2MivTcbQVJXp6W7UgP92WPn3BoVAUF8yKoEJZSEV271nTBEMhq0rtHg0076",
+    domain: ".google.com",
+    path: "/",
+    httpOnly: true,
+    secure: true
+  }
+];
+
 
 async function askGemini(question = "ما هي عاصمة الجزائر؟") {
   console.log("🔥 تم استدعاء askGemini()");
