d6ffee56252417450f34146b845a5bcf0ed68f5d
hani9999 <58763768+hani9999@users.noreply.github.com>
2025-07-12 08:28:05 +0200

    Update and rename puppeteer_script.js to askGemini.js

---
+++ askGemini.js
@@ -0,0 +1,18 @@
+await page.setCookie(
+  {
+    name: "SAPISID",
+    value: "E4ZHqgEnNxmLbN6l/Apj1csRX4-Wo80KJM",
+    domain: ".google.com",
+    path: "/",
+    httpOnly: true,
+    secure: true,
+  },
+  {
+    name: "__Secure-1PSID",
+    value: "AKEyXzXNGzyVJVNBUuVpeYmiWdT3tjI66JnEvx6Uit1AaTuRp0mIzHGCL2FXQ81fqAv4WgvO5-E",
+    domain: ".google.com",
+    path: "/",
+    httpOnly: true,
+    secure: true,
+  }
+);
